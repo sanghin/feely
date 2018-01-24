a06e035ec4d62005bb21ef78b11a5e49ced1b720
Luc Tribolet <luc.tribolet@sycle.net>
2018-01-23 19:51:21 -0800

    :art: Format code

--- app/server.js
+++ app/server.js
@@ -5,7 +5,7 @@ const SHA256 = require('crypto-js/sha256');
 const moment = require('moment-timezone');
 
 const PATH_TO_STATIC_FOLDER = `${__dirname}/../static/img/`;
-const { DISCORD_TOKEN, REDIS_URL } = process.env;
+const {DISCORD_TOKEN, REDIS_URL} = process.env;
 const client = new Discord.Client();
 const TWELVE_HOURS = 43200;
 
@@ -24,7 +24,7 @@ const IS_FEELY_INVOKED = /^!f(eely)?$/;
 /*
  * SETUP SERVER
  */
-const redisClientoptions = REDIS_URL ? { url: REDIS_URL } : {};
+const redisClientoptions = REDIS_URL ? {url: REDIS_URL} : {};
 const redisClient = redis.createClient(redisClientoptions);
 
 client.on('ready', () => {
@@ -52,79 +52,79 @@ const helpMessage = new Discord.RichEmbed()
   .addField("n'est-ce pas ?", "You need the ol' racist uncle card ? Don't move !");
 
 function duplicatedLinkDetection(message) {
-    const hashedUrl = SHA256(message.content).toString();
+  const hashedUrl = SHA256(message.content).toString();
 
-    redisClient.get(hashedUrl, (err, reply) => {
-        if (!reply) {
-            // link is fresh or at least 12 hours old
-            // save the hash and links meta in redis for 12 hours
-            const data = {
-                user: message.author.username,
-                channel: message.channel.name,
-                date: message.createdAt,
-            };
-            redisClient.set(hashedUrl, JSON.stringify(data), 'EX', TWELVE_HOURS);
-        }
+  redisClient.get(hashedUrl, (err, reply) => {
+    if (!reply) {
+      // link is fresh or at least 12 hours old
+      // save the hash and links meta in redis for 12 hours
+      const data = {
+        user: message.author.username,
+        channel: message.channel.name,
+        date: message.createdAt,
+      };
+      redisClient.set(hashedUrl, JSON.stringify(data), 'EX', TWELVE_HOURS);
+    }
 
-        if (reply && message.deletable) {
-            // IT'S A REPOST :SCREAM:
-            // delete the message, send the repost PNG
-            // and blame the user with a custom blame message
-            const data = JSON.parse(reply);
-            const date = moment(new Date(data.date));
-            const blameMessage = `${data.user} posted this in #${data.channel} on ${date.fromNow()}`;
-            message.channel.send(blameMessage, {
-                file: `${PATH_TO_STATIC_FOLDER}/no_repost.jpg`,
-            });
-            message.delete();
-        }
-    });
+    if (reply && message.deletable) {
+      // IT'S A REPOST :SCREAM:
+      // delete the message, send the repost PNG
+      // and blame the user with a custom blame message
+      const data = JSON.parse(reply);
+      const date = moment(new Date(data.date));
+      const blameMessage = `${data.user} posted this in #${data.channel} on ${date.fromNow()}`;
+      message.channel.send(blameMessage, {
+        file: `${PATH_TO_STATIC_FOLDER}/no_repost.jpg`,
+      });
+      message.delete();
+    }
+  });
 }
 
 function invokeDenis(message) {
-    message.channel.send('', {file: `${PATH_TO_STATIC_FOLDER}/ah.png`});
+  message.channel.send('', {file: `${PATH_TO_STATIC_FOLDER}/ah.png`});
 }
 
 function invokeJML(message) {
-    message.channel.send('', {file: `${PATH_TO_STATIC_FOLDER}/nestcepas.gif`});
+  message.channel.send('', {file: `${PATH_TO_STATIC_FOLDER}/nestcepas.gif`});
 }
 
 function invokeTheDonald(message) {
 // will select randomly between fakenews_1.jpg and fakenews_2.jpg
-    const image = ['fakenews_1', 'fakenews_2'][Math.round(Math.random())];
-    message.channel.send('', {file: `${PATH_TO_STATIC_FOLDER}/${image}.jpg`});
+  const image = ['fakenews_1', 'fakenews_2'][Math.round(Math.random())];
+  message.channel.send('', {file: `${PATH_TO_STATIC_FOLDER}/${image}.jpg`});
 }
 
 function invokeJupiter(message) {
-    message.channel.send('', {file: `${PATH_TO_STATIC_FOLDER}/projet.gif`});
+  message.channel.send('', {file: `${PATH_TO_STATIC_FOLDER}/projet.gif`});
 }
 
 function getVancouverTime(message) {
-    const vancouverDateTime = moment()
-        .tz('America/Vancouver')
-        .locale('fr')
-        .format('LT');
-    message.channel.send(`:flag_ca: ${vancouverDateTime} :maple_leaf:`);
+  const vancouverDateTime = moment()
+    .tz('America/Vancouver')
+    .locale('fr')
+    .format('LT');
+  message.channel.send(`:flag_ca: ${vancouverDateTime} :maple_leaf:`);
 }
 
 function avoirHeureParis(message) {
-    const parisDateTime = moment()
-        .tz('Europe/Paris')
-        .locale('fr')
-        .format('LT');
-    message.channel.send(`:flag_fr: ${parisDateTime} :french_bread:`);
+  const parisDateTime = moment()
+    .tz('Europe/Paris')
+    .locale('fr')
+    .format('LT');
+  message.channel.send(`:flag_fr: ${parisDateTime} :french_bread:`);
 }
 
 function getHelpCommand(message) {
-    message.channel.send(helpMessage);
+  message.channel.send(helpMessage);
 }
 
 function invokeTealc(message) {
-    message.channel.send('', {file: `${PATH_TO_STATIC_FOLDER}/indeed.gif`});
+  message.channel.send('', {file: `${PATH_TO_STATIC_FOLDER}/indeed.gif`});
 }
 
 function invokeGoodMan(message) {
-    message.channel.send('', {file: `${PATH_TO_STATIC_FOLDER}/so_good.png`});
+  message.channel.send('', {file: `${PATH_TO_STATIC_FOLDER}/so_good.png`});
 }
 
 /*
@@ -157,11 +157,11 @@ client.on('message', (message) => {
   }
 
   if (message.content.match(IS_GET_VANCOUVER_TIME_REGEX)) {
-      getVancouverTime(message);
+    getVancouverTime(message);
   }
 
   if (message.content.match(IS_GET_PARIS_TIME_REGEX)) {
-      avoirHeureParis(message);
+    avoirHeureParis(message);
   }
 
   if (message.content.match(IS_FEELY_INVOKED)) {
